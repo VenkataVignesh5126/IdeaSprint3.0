95490d0d68484261e0c9d2bbc3f1e7d9e05651c8
kruthi priyanka <kruthipriyanka07@gmail.com>
2026-03-26 19:49:40 +0530

    added scanner module

---
+++ scanner.py
@@ -0,0 +1,18 @@
+import socket
+
+def scan_ports(target):
+    ports = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3389]
+    open_ports = []
+
+    for port in ports:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+
+        result = s.connect_ex((target, port))
+
+        if result == 0:
+            open_ports.append(port)
+
+        s.close()
+
+    return open_ports
